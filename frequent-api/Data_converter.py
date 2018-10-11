55655cffeeb579e10b4ebdc36d59a9af0763bbb1
Thanh Thai <flashback21196@gmail.com>
2018-10-11 15:17:44 +0700

    First commit

---
+++ frequent-api/Data_converter.py
@@ -0,0 +1,77 @@
+import json
+import pickle
+
+
+def dataconvertFP(b):
+    retDict = {}
+    sentence = []
+    list=[]
+    for tags in b:
+        a = tags["tags"]
+        for tag, value in a.items():
+            for item in value:
+                list.append(item+':'+tag)
+        sentence.append(list)
+        list = []
+    for trans in sentence:
+        retDict[frozenset(trans)] = 1
+    return retDict
+
+def dataconvertIT(b, count=1, retDict={}):
+    sentence = []
+    list = []
+    for tags in b:
+        a = tags["tags"]
+        for tag, value in a.items():
+            for item in value:
+                list.append(item + ':' + tag)
+        sentence.append(list)
+        list = []
+    #print(sentence)
+    for trans in sentence:
+        for item in trans:
+            if (frozenset([item]) in retDict):
+                temp = retDict[frozenset([item])]
+                retDict[frozenset([item])] = temp.union({count})
+            else:
+                retDict[frozenset([item])] = {count}
+        print(count)
+        count += 1
+
+    return retDict, count
+
+
+def convertIT(dataRaw,con=False):
+
+    if (con):
+        with open('current.pkl', 'rb') as curr:
+            count = pickle.load(curr)
+            with open('Convert_data_for_It.pkl', 'rb') as file:
+                data = pickle.load(file)
+                data, count = dataconvertIT(dataRaw,count,data)
+                with open('Convert_data_for_It.pkl', 'wb') as file:
+                    pickle.dump(data, file)
+                with open('current.pkl', 'wb') as curr:
+                    pickle.dump(count, curr)
+    else:
+        data, count = dataconvertIT(dataRaw)
+        with open('Convert_data_for_It.pkl', 'wb') as file:
+            pickle.dump(data, file)
+        with open('current.pkl', 'wb') as curr:
+            pickle.dump(count,curr)
+
+def convertFP(dataRaw):
+    data = dataconvertFP(dataRaw)
+    with open('Convert_data_for_Fp.pkl','wb') as file:
+        pickle.dump(data,file)
+
+dataRaw = []
+
+with open('fullBDS.json', 'r', encoding='utf8') as infile: #change data location
+    dataRaw = json.load(infile)
+    infile.close()
+
+
+
+#convertIT(dataRaw) #unblock to convert data to IT input data
+convertFP(dataRaw) #unblock to convert data to FP input data
---
+++ frequent-api/README.txt
@@ -0,0 +1,11 @@
+go to this link to download fullBDS.json
+
+https://1drv.ms/u/s!An1UosdcVaRLiFUjYAyme71KiZRD
+
+run Data_converter.py to generate pkl file
+
+run FP-Tree.py or IT-Tree.py to generate frequent itemsets change c value in minSupcal() function to change minsup 
+	Input of FP-Tree: Convert_data_for_Fp.pkl
+	Input of IT-Tree: Convert_data_for_It.pkl + current.pkl
+
+run api.py (frequentItemsFP.json as default)
